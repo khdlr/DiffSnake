17337638da628827b648fa0c92fbccd76b3ca725
Konrad Heidler <k.heidler@tum.de>
2022-12-14 13:12:32 +0100

    Fast Xception backbone

--- lib/models/backbones/__init__.py
+++ lib/models/backbones/__init__.py
@@ -1,6 +1,7 @@
 from .resnet import ResNet18, ResNet34, ResNet50, ResNet101, ResNet152, ResNet200
 from .slim_resnet import SlimResNet18, SlimResNet34, SlimResNet50, SlimResNet101, SlimResNet152, SlimResNet200
 from .xception import Xception, TruncatedXception
+from .fast_xception import FastXception
 from .calfin_xception import CALFINXception
 from .nextception import Nextception
 from .convnext import ConvNeXt
